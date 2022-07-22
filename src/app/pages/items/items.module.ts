091e0269774a941f48aa2b4cbb8eade3c07216df
tdp1999 <tdp99.business@gmail.com>
2022-07-22 17:04:12 +0700

    create project structure + add angular material

---
+++ src/app/pages/items/items.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ItemsComponent } from './items.component';
+
+
+
+@NgModule({
+  declarations: [
+    ItemsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ItemsModule { }
